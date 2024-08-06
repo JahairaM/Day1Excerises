933250b190c06c5c539fb1a0ee6778f37a4e36a0
Jahaira Maxwell <Maxawell@msu.edu>
2024-08-05 21:38:40 -0400

    Initial commit

---
+++ Day1.py
@@ -0,0 +1,33 @@
+#Excerise 1
+favsong = input("What's your favorite song?")
+print(favsong)
+
+#Excerise 2
+Number = (input("What is your favorite number?"))
+sum = int(Number) + int(1)
+print(sum)
+
+#Excerise 3
+number = input("Enter a number")
+sum = int(number) + float(0.5)
+print(sum)
+
+#Excerise 4
+number1 = input("Enter a number with a decimal")
+number2 = input("Enter a number with a decimal")
+sum = float(number1) + float(number2)
+print(sum)
+
+#Excerise 5
+number0 = input("Enter a number")
+number0 = int(number0)
+number3 = input("Enter a number")
+number3 = int(number3)
+print(number0 * number3)
+
+#Excerise 6
+you = input('enter a number')
+you = int(you)
+again = input('enter another number')
+again = int(again)
+print(you / again)
